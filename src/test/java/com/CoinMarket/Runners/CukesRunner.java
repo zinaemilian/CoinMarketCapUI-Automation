9f43ea3c27d158c0c2ded9bedfe2ef7e7f48b6d7
Z. Emiliyanova <82812872+zinaemilian@users.noreply.github.com>
2022-12-07 08:50:05 +0000

    Update CukesRunner.java
    updated

--- src/test/java/com/CoinMarket/Runners/CukesRunner.java
+++ src/test/java/com/CoinMarket/Runners/CukesRunner.java
@@ -9,8 +9,8 @@ import org.junit.runner.RunWith;
         plugin = {"json:target/cucumber.json",
                 "html:target/default-html-reports",
                 "rerun:target/rerun.txt"},
-        features = "src/test/resources/features",
-        glue = "/step_definitions",
+        features = "src/test/resources/Features",
+        glue = "/Step_Definitions",
         dryRun = false,
       tags = "@Smoke"
 )
