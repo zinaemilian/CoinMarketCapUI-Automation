46393b5375bcbc586afb9fcfed78ab028535283d
Z. Emiliyanova <82812872+zinaemilian@users.noreply.github.com>
2022-12-07 21:42:57 +0000

    Update CukesRunner.java
    added path for glue

--- src/test/java/com/CoinMarket/Runners/CukesRunner.java
+++ src/test/java/com/CoinMarket/Runners/CukesRunner.java
@@ -4,15 +4,18 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"json:target/cucumber.json",
-                "html:target/default-html-reports",
+      plugin = {"json:target/cucumber.json",
+              "html:target/default-html-reports",
                 "rerun:target/rerun.txt"},
-        features = "src/test/resources/Features",
-        glue = "/Step_Definitions",
-        dryRun = false,
-      tags = "@Smoke"
+       features = "src/test/resources/Features",
+       glue = "com/CoinMarket/Step_Definitions",
+        dryRun = true,
+        tags = "@Smoke",
+        publish = true
 )
 public class CukesRunner {
+
 }
